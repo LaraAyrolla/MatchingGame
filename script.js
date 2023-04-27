7f49d50abb782c94389f6929a1bac7e483819d82
Lara Ayrolla <lara@pop-os.localdomain>
2023-04-26 23:31:54 -0300

    randomizing card positions

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 //contants
 const livesFile = "assets/heart.png";
 const livesClass = "lives";
+const livesId = "lives";
 const lifeClass = "life";
+const lifeId = "life";
 const livesNumber = 10;
+const backgroundFile = "assets/background.png";
+const cardsClass = "cards";
+const cardsId = "cards";
+const cardsNumber = 12;
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 //"global" variables
 var card1, card2, count=0, attempts=0, lives=10, pairs=0, id2;
@@ -13,6 +20,9 @@ pairs = parseInt(pairs);
 
 window.onload = function initializeGameBoard(){
     setLives();
+    cardsArray = numbersArray;
+    randomizeCardPositions(cardsArray);
+    setCards(cardsArray);
 }
 
 function setLives(){
@@ -20,20 +30,38 @@ function setLives(){
     for(i=0; i<livesNumber; i++){
         var life = document.createElement("img");
         life.src = livesFile;
-        life.id = String(i);
+        life.id = lifeId+String(i);
         life.className = lifeClass;
-        document.getElementById(livesClass).appendChild(life);
+        document.getElementById(livesId).appendChild(life);
     }
 }
 
-function setCards(){
+function setCards(cardsArray){
+    for(i=0; i<cardsNumber; i++){
+        value = cardsArray[i];
+        halfCards =  cardsNumber/2;
 
+        card = document.createElement("img");
+        card.src = backgroundFile;
+
+        card.className = value <= halfCards
+            ? String(value)
+            : String(value-halfCards)
+        ;
+
+        card.id = value <= halfCards
+            ? String(value)
+            : String(value-halfCards)+"2"
+        ;
+
+        document.getElementById(cardsId).appendChild(card);
+    }
 }
 
-function randomizeCardPositions(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+function randomizeCardPositions(cardsArray) {
+    for (i=cardsArray.length-1; i>0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        [cardsArray[i], cardsArray[j]] = [cardsArray[j], cardsArray[i]];
     }
 }
 
@@ -46,7 +74,7 @@ document.addEventListener ('click', function (event){
     var id = event.target.id;
 
     //checks if the click happened within the are of one of the cards
-    if ((classe=='a'||classe=='b'||classe=='c'||classe=='d'||classe=='e'||classe=='f')&&(id!=id2)){
+    if (parseInt(classe)>=1 && parseInt(classe)<=cardsNumber/2 && id!=id2){
         //counter to check if it's the first or second card of the round
         count++;
 
@@ -108,10 +136,10 @@ function increaseCorrectPairs(){
 function reduceLives(){
     lives--;
 
-    document.getElementById(String(lives)).style.display="none";
+    document.getElementById(lifeId+String(lives)).style.display="none";
 
     if(lives==0){
-        document.getElementById("0").style.display="none";
+        document.getElementById(lifeId+"0").style.display="none";
         //if the lives end
         document.getElementById("end-game").innerText = "GAME OVER!";
         document.getElementById("end-game").style.color = "red";
